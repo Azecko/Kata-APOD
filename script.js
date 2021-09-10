c3e9582de089b9e3068fda913b37c10503024c79
Sami Panseri <sami.panseri@epfl.ch>
2021-09-10 15:55:36 +0200

    Made the right arrow work
    - Refactor the APOD HTML
    - Can support responsive videos

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ function getapod(date) {
   $.get(
     `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`,
     function (data) {
-      $(".result").html(
-        `<h1 class="apod-title">${data.title}</h1><p class="apod-date">${data.date}</p><a href=${data.hdurl} target="_blank"><img src=${data.url} class="apod-image"></img></a><p class="apod-description">${data.explanation}</p>`
-      );
+      $(document).prop("title", `APOD: ${data.title}`);
+      let APODHTML = `<h1 class="apod-title">${data.title}</h1>
+                    <p class="apod-date">${data.date}</p>`;
+
+      if (data.media_type == "image") {
+        APODHTML += `<a href=${data.hdurl} target="_blank">
+          <img src=${data.url} class="apod-image">
+      </a>`;
+      } else if (data.media_type == "video") {
+        APODHTML += `<div class="video-container">
+          <iframe src=${data.url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>`;
+      } else {
+        console.error("Désolé, je ne sais faire que des images et des vidéos");
+      }
+      APODHTML += `<p class="apod-description">${data.explanation}</p>`;
+      $(".result").html(APODHTML);
     }
   );
 }
@@ -16,9 +30,19 @@ function manageclick() {
   currentdate.setDate(currentdate.getDate() - 1);
 
   let daybefore = currentdate.toISOString().slice(0, 10);
-  console.log(daybefore);
 
   getapod(daybefore);
+}
+
+function managerightclick() {
+  var apoddate = $(".apod-date").text();
+
+  let currentdate = new Date(apoddate);
+  currentdate.setDate(currentdate.getDate() + 1);
+
+  let dayafter = currentdate.toISOString().slice(0, 10);
+
+  getapod(dayafter);
 }
 
 $(document).ready(function () {
@@ -27,10 +51,37 @@ $(document).ready(function () {
 
   $(".left-arrow").click(function () {
     manageclick();
+
+    if ($(".right-arrow").css("opacity") == "0.1") {
+      $(".right-arrow").css("opacity", "1");
+      $(".right-arrow").css("cursor", "pointer");
+    }
   });
 
   $(".right-arrow").css("opacity", "0.1");
   $(".left-arrow").css("cursor", "pointer");
+
+  $(".right-arrow").click(function () {
+    let apoddate = $(".apod-date").text();
+    let currentdate = new Date(apoddate);
+
+    if (date == currentdate.toISOString().slice(0, 10)) {
+      $(".right-arrow").css("opacity", "0.1");
+      $(".right-arrow").css("cursor", "default");
+    } else {
+      managerightclick();
+      let olddate = new Date($(".apod-date").text());
+      olddate.setDate(olddate.getDate() + 1);
+
+      let newdate = olddate.toISOString().slice(0, 10);
+      if (date == newdate) {
+        $(".right-arrow").css("opacity", "0.1");
+        $(".right-arrow").css("cursor", "default");
+      } else {
+        managerightclick();
+      }
+    }
+  });
 
   //console.log(body)
 });
